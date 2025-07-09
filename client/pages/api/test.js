68f0464ffd3d6b7df001f248b7135e1122962161
Juli Khosla <juli@spotonautomation.com>
2025-07-09 11:07:59 -0700

    Removed test file

--- client/pages/api/test.js
+++ client/pages/api/test.js
@@ -1,6 +1,6 @@
-import { getProducts } from "@/lib/shopify";
+// import { getProducts } from "@/lib/shopify";
 
 export default async function handler(req, res) {
-	const data = await getProducts();
-	res.status(200).json(data);
+	// const data = await getProducts();
+	// res.status(200).json(data);
 }
